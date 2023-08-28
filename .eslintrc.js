892c93251574b9fae33f149039ba37780f29c7ce
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-08-29 06:17:25 +0700

    ok

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off' // 关闭组件名必须是多个单词的形式
   }
 }
