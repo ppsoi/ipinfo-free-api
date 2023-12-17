84fc5706631454ab5d3df2344e9d5d4cbe1131e7
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-12-18 06:08:30 +0700

    chore: Refactor API middleware for CORS handling
    This commit refactors the `onRequest` function in the API middleware to improve Cross-Origin Resource Sharing (CORS) handling. It makes the following changes:
    
    - Adds comments for better code readability.
    - Extracts and modifies headers related to CORS.
    - Fixes an issue with extracting the client IP address from the URL path.
    - Constructs a modified request object and sends it to the desired endpoint.
    - Waits for and captures the response.
    - Removes unnecessary response headers.
    
    These changes aim to enhance the security and reliability of the API middleware by properly handling CORS-related headers and ensuring correct IP address extraction.

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,18 +1,33 @@
 export async function onRequest(context) {
-    // eslint-disable-next-line no-unused-vars
-    const { request, waitUntil } = context;
+    const {
+        request,       // 原始请求对象，包含客户端的请求信息
+        env,           // Worker 环境变量，类似于服务器配置
+        params,        // URL 路径参数，例如从文件名中解析而来的参数
+        waitUntil,     // 用于延迟处理完成的 Promise，以确保在发送响应前完成
+        next,          // 用于调用下一个中间件或获取资源
+        data,          // 中间件之间传递数据的容器
+    } = context;
 
+    // 解析请求 URL
     const url = new URL(request.url);
+
+    // 获取允许跨域访问的 Origin
     const headers_Origin = request.headers.get("Access-Control-Allow-Origin") || "*";
 
+    // 复制原始请求的 headers，并添加自定义 headers
     const myHeaders = new Headers(request.headers);
+    const path = url.pathname
+    // 删除原来的 Referer header
     myHeaders.delete("Referer");
+    // 删除原来的 Origin header
     myHeaders.delete("Origin");
+    // 添加新的 Origin header
     myHeaders.append("Origin", "https://ipinfo.io/");
+    // 添加新的 Referer header
     myHeaders.append("Referer", "https://ipinfo.io/");
-
-    let getip;
-    if (url.pathname.startsWith("/api")) {
+    let getip
+    if (path == "/api" || path == "/api/") {
+        // 提取客户端 IP 地址
         getip =
             request.headers.get('CF-Connecting-IP') ||
             request.headers.get('X-Forwarded-For') ||
@@ -23,30 +38,31 @@ export async function onRequest(context) {
             request.headers.get('Forwarded-For') ||
             request.headers.get('Forwarded');
     } else {
-        getip = url.pathname.replace("/api/", "");
+        getip = path.replace("/api/", "")
     }
-
-    const modifiedRequest = new Request(`https://ipinfo.io/widget/demo/${getip}${url.search}`, {
+    // 构建修改后的请求对象
+    const modifiedRequest = new Request('https://ipinfo.io/widget/demo/' + getip + url.search, {
         method: request.method,
         headers: myHeaders,
         body: request.body,
     });
 
+    // 发起修改后的请求并等待响应
     const response = await fetch(modifiedRequest);
 
+    // 复制响应内容及其 headers 到新的响应对象
     const modifiedResponse = new Response(response.body, response);
 
+    // 设置允许跨域访问的 Origin
     modifiedResponse.headers.set('Access-Control-Allow-Origin', headers_Origin);
 
-    const headersToDelete = [
-        'x-proxied-host',
-        'x-proxied-path',
-        'x-proxied-id',
-        'proxy-domain',
-        'Link',
-        'via'
-    ];
-    headersToDelete.forEach(header => modifiedResponse.headers.delete(header));
+    // 删除一些不需要的响应 headers
+    modifiedResponse.headers.delete('x-proxied-host');
+    modifiedResponse.headers.delete('x-proxied-path');
+    modifiedResponse.headers.delete('x-proxied-id');
+    modifiedResponse.headers.delete('proxy-domain');
+    modifiedResponse.headers.delete('Link');
+    modifiedResponse.headers.delete('via');
 
-    return modifiedResponse;
+    return modifiedResponse; // 返回修改后的响应
 }
