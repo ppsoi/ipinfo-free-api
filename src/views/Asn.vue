c3cf5c95da838c20700df6ef68c29c1b274afd13
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-12-18 07:27:43 +0700

    feat(asn-view): improve ASN view UI and data display
    Refactored the ASN.vue file to enhance the user interface and improve the representation of ASN information. The changes include:
    
    - Removed commented out code and unnecessary CSS styles.
    - Modified table headers to have capitalized text.
    - Created a helper method, `removeJson()`, to remove empty values and "[]" from displayed table cells.
    - Implemented the `parseValues()` method to generate clickable links for URLs in the Peer table cells.
    - Updated the URL used for IP lookup to a reliable API endpoint.
    - Refactored the `lookupIpAddress()` method to handle input ASN without the "AS" prefix.
    - Saved entered ASN to a cookie to retain it for subsequent visits.
    
    These changes provide a cleaner and more user-friendly ASN view, ensuring consistent and accurate display of ASN information.

--- src/views/Asn.vue
+++ src/views/Asn.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="container asn is-fullhd">
-
-    <!-- <div class="notification is-primary">
-      This container is <strong>centered</strong> on desktop and larger viewports.
-    </div> -->
     <div class="ip-lookup">
       <section class="section my-ip-lookup">
         <div class="container">
@@ -19,21 +15,17 @@
               <button class="button is-primary" @click="lookupIpAddress">Lookup</button>
             </div>
           </div>
-
-          <!-- <div class="result"> -->
           <div class="table-container">
             <h2 class="subtitle">ASN INFO</h2>
-
             <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth" v-if="ipInfo">
               <tbody>
                 <tr v-for="(value, key) in ipInfo" :key="key">
-                  <th style="  text-transform: capitalize;">{{ key }}</th>
-                  <td>{{ value }}</td>
+                  <th style="text-transform: capitalize">{{ key }}</th>
+                  <td>{{ removeJson(value) }}</td>
                 </tr>
               </tbody>
             </table>
             <h2 class="subtitle">IPv4</h2>
-
             <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth" v-if="cidrv4">
               <thead>
                 <tr>
@@ -48,18 +40,17 @@
               </thead>
               <tbody>
                 <tr v-for="(prefix, index) in cidrv4" :key="index">
-                  <td>{{ prefix.netblock }}</td>
-                  <td>{{ prefix.id }}</td>
-                  <td>{{ prefix.name }}</td>
-                  <td>{{ prefix.country }}</td>
-                  <td>{{ prefix.size }}</td>
-                  <td>{{ prefix.status }}</td>
-                  <td>{{ prefix.domain }}</td>
+                  <td>{{ removeJson(prefix.netblock) }}</td>
+                  <td>{{ removeJson(prefix.id) }}</td>
+                  <td>{{ removeJson(prefix.name) }}</td>
+                  <td>{{ removeJson(prefix.country) }}</td>
+                  <td>{{ removeJson(prefix.size) }}</td>
+                  <td>{{ removeJson(prefix.status) }}</td>
+                  <td>{{ removeJson(prefix.domain) }}</td>
                 </tr>
               </tbody>
             </table>
             <h2 class="subtitle">IPv6</h2>
-
             <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth" v-if="cidrv6">
               <thead>
                 <tr>
@@ -74,42 +65,37 @@
               </thead>
               <tbody>
                 <tr v-for="(prefix, index) in cidrv6" :key="index">
-                  <td>{{ prefix.netblock }}</td>
-                  <td>{{ prefix.id }}</td>
-                  <td>{{ prefix.name }}</td>
-                  <td>{{ prefix.country }}</td>
-                  <td>{{ prefix.size }}</td>
-                  <td>{{ prefix.status }}</td>
-                  <td>{{ prefix.domain }}</td>
+                  <td>{{ removeJson(prefix.netblock) }}</td>
+                  <td>{{ removeJson(prefix.id) }}</td>
+                  <td>{{ removeJson(prefix.name) }}</td>
+                  <td>{{ removeJson(prefix.country) }}</td>
+                  <td>{{ removeJson(prefix.size) }}</td>
+                  <td>{{ removeJson(prefix.status) }}</td>
+                  <td>{{ removeJson(prefix.domain) }}</td>
                 </tr>
               </tbody>
             </table>
             <h2 class="subtitle">Peers</h2>
-
             <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth" v-if="peerinfo">
               <tbody>
-                <tr v-for="(value, key) in peerinfo" :key="key">
+                <tr v-for="(value, key) in removeJson(peerinfo)" :key="key">
                   <td>{{ key }}</td>
-                  <td>{{ value }}</td>
+                  <!-- <td>{{ value }}</td> -->
+                  <td>
+                    <span v-html="parseValues(value)"></span>
+                  </td>
                 </tr>
               </tbody>
             </table>
-            <!-- <table v-if="ipInfo">
-              <tr v-for="(value, key) in ipInfo" :key="key">
-                <td>{{ key }}</td>
-                <td>{{ value }}</td>
-              </tr>
-            </table> -->
           </div>
         </div>
-        <!-- </div> -->
       </section>
       <footer class="footer">
         <div class="content has-text-centered">
           <p>
             <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is licensed
+            <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
           </p>
         </div>
       </footer>
@@ -117,27 +103,14 @@
   </div>
 </template>
 
-<style scoped>
-.my-ip-lookup {
-  padding: 20px;
-}
-
-.asn {
-  margin-top: 50px;
-  margin-left: 50px;
-  margin-right: 50px;
-}
-</style>
-
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
-      ipAddress: this.getASNAddressFromCookie() || '', // Use the stored ASN from the cookie
+      ipAddress: this.getASNAddressFromCookie() || "", // Use the stored ASN from the cookie
       ipInfo: null,
-      // cidrs: null,
       cidrv4: null,
       cidrv6: null,
       peerinfo: null,
@@ -151,19 +124,15 @@ export default {
     }
     this.lookupIpAddress();
   },
-
   methods: {
     async lookupIpAddress() {
       try {
-        // 处理输入的 ASN
-        if (this.ipAddress.toLowerCase().startsWith('as')) {
+        if (this.ipAddress.toLowerCase().startsWith("as")) {
           this.ipAddress = this.ipAddress.substring(2);
         }
-        // const url = `https://${this.hostname}/api/${this.ipAddress}`;
         const url = `https://ip.3k.free.hr/api/AS${this.ipAddress}`;
         const response = await axios.get(url);
         this.ipInfo = response.data;
-        // 构建剩余字段的新对象
         this.ipInfo = {
           asn: this.ipInfo.asn,
           name: this.ipInfo.name,
@@ -175,34 +144,49 @@ export default {
           type: this.ipInfo.type,
         };
         this.peerinfo = {
-          peers: response.data.peers,
-          upstreams: response.data.upstreams,
-          downstreams: response.data.downstreams,
+          peers: this.removeJson(response.data.peers),
+          upstreams: this.removeJson(response.data.upstreams),
+          downstreams: this.removeJson(response.data.downstreams),
         };
         this.cidrv4 = response.data.prefixes;
         this.cidrv6 = response.data.prefixes6;
-        // this.cidrs = {
-        //   ipv4: this.ipv4,
-        //   ipv6: this.ipv6,
-        // }
-        // Save the entered ASN to the cookie
         this.saveASNAddressToCookie(this.ipAddress);
       } catch (error) {
-        console.error('Error fetching IP info:', error);
+        console.error("Error fetching IP info:", error);
         this.ipInfo = null;
       }
     },
     saveASNAddressToCookie(ipAddress) {
       const now = new Date();
-      now.setFullYear(now.getFullYear() + 1); // Set the cookie expiration to 1 year from now
+      now.setFullYear(now.getFullYear() + 1);
       document.cookie = `ASNAddress=${ipAddress}; expires=${now.toUTCString()}; path=/`;
     },
     getASNAddressFromCookie() {
-      const cookie = document.cookie.split('; ').find(row => row.startsWith('ASNAddress='));
+      const cookie = document.cookie.split("; ").find((row) => row.startsWith("ASNAddress="));
       if (cookie) {
-        return cookie.split('=')[1];
+        return cookie.split("=")[1];
       }
       return null;
+    },
+    removeJson(item) {
+      if (Array.isArray(item)) {
+        item = item.filter((value) => value !== "" && value !== "[]");
+        return item.join(", ");
+      } else {
+        if (item === "" || item === "[]") {
+          return "-";
+        }
+        return item;
+      }
+    },
+    parseValues(value) {
+      const values = this.removeJson(value);
+      if (Array.isArray(values)) {
+        const links = values.map(val => `<a href="${val}" target="_blank">${val}</a>`);
+        return links.join(", ");
+      } else {
+        return values;
+      }
     },
   },
 };
